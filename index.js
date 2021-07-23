a29dc02ac94ea8aabdb098c150796bda032d78c1
Donovan Rowzee <44952478+drowzee001@users.noreply.github.com>
2021-07-22 20:58:08 -0700

    updated cors

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "https://main.d2um1sk5kilrcy.amplifyapp.com",
+    origin: "https://messaging-web-app.donovanrowzee.net",
     credentials: true,
   })
 );
